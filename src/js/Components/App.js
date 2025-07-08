cba414624fd1a7128225d130d90c2392b467ad2b
ninjabear <nino.ostir@gmail.com>
2025-07-08 12:26:12 +0200

    components data update in App.js, Header.js, HourlyForecast.js and Widget.js

--- src/js/Components/App.js
+++ src/js/Components/App.js
@@ -1,11 +1,13 @@
 import React from "react";
 import styles from "../../styles/main.scss";
 import Widget from "./WeatherWidget/Widget";
+import data from "../data/weather";
+console.log(data);
 const App = () => {
     return (
         <>
-            <h1>Test</h1>
-            <Widget>
+            <h1>Weather Forecast Widget</h1>
+            <Widget data={data[0]}>
 
             </Widget>
         </>
--- src/js/Components/WeatherWidget/Widget.js
+++ src/js/Components/WeatherWidget/Widget.js
@@ -3,10 +3,12 @@ import styles from "./Widget.scss";
 import Header from "./Header";
 import HourlyForecast from "./HourlyForecast";
 const Widget = (props) => {
+    const {city, current_temperature, state} = props.data;
+    const {hourly} = props.data;
     return (
         <section className="widget__section">
-            <Header></Header>
-            <HourlyForecast></HourlyForecast>
+            <Header city={city} temp={current_temperature} state={state}></Header>
+            <HourlyForecast forecast={hourly}/>
         </section>
     )
 }
